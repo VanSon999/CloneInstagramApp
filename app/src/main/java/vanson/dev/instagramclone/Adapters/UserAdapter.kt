fa447deff3ecd2f1b8c69c4acb515096a6c91f83
VanSon999 <nguyenvanson95999@gmail.com>
2021-01-01 22:08:39 +0700

    Search User

--- app/src/main/java/vanson/dev/instagramclone/Adapters/UserAdapter.kt
+++ app/src/main/java/vanson/dev/instagramclone/Adapters/UserAdapter.kt
@@ -1,4 +1,44 @@
 package vanson.dev.instagramclone.Adapters
 
-class UserAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.annotation.NonNull
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import de.hdodenhof.circleimageview.CircleImageView
+import vanson.dev.instagramclone.Models.User
+import vanson.dev.instagramclone.R
+
+class UserAdapter (private var mContext : Context, private var mUser: List<User>, private var isFragment: Boolean = false) : RecyclerView.Adapter<UserAdapter.ViewHolder>() {
+
+    class ViewHolder(@NonNull itemView: View) : RecyclerView.ViewHolder(itemView){
+        var userName: TextView = itemView.findViewById(R.id.user_name_search)
+        var userFullName: TextView = itemView.findViewById(R.id.user_full_name_search)
+        var userProfileImage: CircleImageView = itemView.findViewById(R.id.user_profile_image_search)
+        var followButton: Button = itemView.findViewById(R.id.follow_btn_search)
+
+        fun bindUser(user: User){
+            userName.text = user.getUsername()
+            userFullName.text = user.getFullName()
+            Picasso.get().load(user.getImage()).placeholder(R.drawable.profile).into(userProfileImage)
+        }
+
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserAdapter.ViewHolder {
+        val view = LayoutInflater.from(mContext).inflate(R.layout.user_item_layout, parent, false)
+        return UserAdapter.ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return mUser.size
+    }
+
+    override fun onBindViewHolder(holder: UserAdapter.ViewHolder, position: Int) {
+        holder.bindUser(mUser[position])
+    }
+
 }
--- app/src/main/java/vanson/dev/instagramclone/Fragment/SearchFragment.kt
+++ app/src/main/java/vanson/dev/instagramclone/Fragment/SearchFragment.kt
@@ -1,11 +1,24 @@
 package vanson.dev.instagramclone.Fragment
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.fragment_search.view.*
+import vanson.dev.instagramclone.Adapters.UserAdapter
+import vanson.dev.instagramclone.Models.User
 import vanson.dev.instagramclone.R
+import java.util.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,6 +31,10 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class SearchFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private var userAdapter: UserAdapter? = null
+    private lateinit var mUser: ArrayList<User>
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +52,82 @@ class SearchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        val view =  inflater.inflate(R.layout.fragment_search, container, false)
+
+        recyclerView = view.findViewById(R.id.recycler_view_search)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        //for fragment, context can null, so you have to use context.let to check it
+        mUser = ArrayList()
+        userAdapter = context?.let { UserAdapter(it, mUser, true) }
+        recyclerView.adapter = userAdapter
+
+        view.search_edit_text.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                Log.d("Test", "Change")
+                if(view.search_edit_text.text.toString() != ""){
+                    Log.d("Test", "Text : ${p0.toString()}")
+                    recyclerView.visibility = View.VISIBLE
+                    retrieveUsers()
+                    searchUser(p0.toString())
+                }
+            }
+        })
+        return view
+    }
+
+    private fun searchUser(str: String) {
+        val query = FirebaseDatabase.getInstance().getReference().child("Users").orderByChild("fullname").startAt(str.toUpperCase()).endAt(str.toLowerCase() + "\uf8ff")
+
+        query.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                mUser.clear()
+
+                for(x in snapshot.children){
+                    val user = x.getValue(User::class.java)
+                    Log.d("Test", "Search -- " + user)
+                    if(user != null){
+                        mUser.add(user)
+                    }
+                }
+
+                userAdapter?.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
+    }
+
+    private fun retrieveUsers() {
+        val userRef = FirebaseDatabase.getInstance().getReference().child("Users")
+        userRef.addValueEventListener(object : ValueEventListener{
+            override fun onCancelled(error: DatabaseError) {
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for(x in snapshot.children){
+                    val temp = x.getValue(User::class.java)
+                    Log.d("Test", "Retrieve -- " + temp)
+                }
+                if(view?.search_edit_text?.text.toString() == ""){
+                    mUser.clear()
+                    for(x in snapshot.children){
+                        val user = x.getValue(User::class.java)
+                        if(user != null){
+                            mUser.add(user)
+                        }
+                    }
+                    userAdapter?.notifyDataSetChanged()
+                }
+            }
+        })
     }
 
     companion object {
