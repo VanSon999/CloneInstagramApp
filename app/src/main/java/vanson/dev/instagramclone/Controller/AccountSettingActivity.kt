de0856f478504cd222074e0219f7a3628fbb7a31
VanSon999 <nguyenvanson95999@gmail.com>
2021-01-01 16:35:49 +0700

    Sign In and Log Out

--- app/src/main/java/vanson/dev/instagramclone/Controller/AccountSettingActivity.kt
+++ app/src/main/java/vanson/dev/instagramclone/Controller/AccountSettingActivity.kt
@@ -1,12 +1,25 @@
 package vanson.dev.instagramclone.Controller
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_account_setting.*
 import vanson.dev.instagramclone.R
 
 class AccountSettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_setting)
+
+
+        logout_btn_profile_frag.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this, SignInActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/vanson/dev/instagramclone/Controller/SignInActivity.kt
+++ app/src/main/java/vanson/dev/instagramclone/Controller/SignInActivity.kt
@@ -1,8 +1,11 @@
 package vanson.dev.instagramclone.Controller
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_in.*
 import vanson.dev.instagramclone.R
@@ -14,6 +17,42 @@ class SignInActivity : AppCompatActivity() {
 
         signUp_link_btn.setOnClickListener {
             startActivity(Intent(this,SignUpActivity::class.java))
+        }
+
+        login_btn.setOnClickListener {
+            loginUser()
+        }
+    }
+
+    private fun loginUser() {
+        val email = email_login.text.toString()
+        val password = password_login.text.toString()
+
+        when{
+            TextUtils.isEmpty(email) -> Toast.makeText(this,"Email is required", Toast.LENGTH_LONG).show()
+            TextUtils.isEmpty(password) -> Toast.makeText(this,"Password is required", Toast.LENGTH_LONG).show()
+            else -> {
+                val progressDialog = ProgressDialog(this) //Note to it!
+                progressDialog.setTitle("SignIn")
+                progressDialog.setMessage("Please wait, this may take a while...")
+                progressDialog.setCanceledOnTouchOutside(false)
+                progressDialog.show()
+
+                val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                    if(task.isSuccessful){
+                        progressDialog.dismiss()
+                        val intent = Intent(this, MainActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                        startActivity(intent)
+                        finish()
+                    }else{
+                        progressDialog.dismiss()
+                        Toast.makeText(this,"ERROR: ${task.exception.toString()}", Toast.LENGTH_LONG).show()
+                        FirebaseAuth.getInstance().signOut()
+                    }
+                }
+            }
         }
     }
 
