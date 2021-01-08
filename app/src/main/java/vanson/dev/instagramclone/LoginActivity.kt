cbefa856ca8de92eacb5f05be8c63d91aaca079e
VanSon999 <nguyenvanson95999@gmail.com>
2021-01-08 20:32:53 +0700

    beauty login activity

--- app/src/main/java/vanson/dev/instagramclone/LoginActivity.kt
+++ app/src/main/java/vanson/dev/instagramclone/LoginActivity.kt
@@ -2,11 +2,44 @@ package vanson.dev.instagramclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import kotlinx.android.synthetic.main.activity_login.*
+import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEvent
+import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEventListener
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), KeyboardVisibilityEventListener,
+    TextWatcher {
     private val TAG = "LoginActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        KeyboardVisibilityEvent.setEventListener(this, this)
+        login_btn.isEnabled = false
+
+        email_login.addTextChangedListener(this)
+        password_login.addTextChangedListener(this)
+    }
+
+    override fun onVisibilityChanged(isKeyBoardOpen: Boolean) {
+        if(isKeyBoardOpen){
+            scroll_view.scrollTo(0, scroll_view.bottom)
+            create_account_text.visibility = View.GONE
+        }else{
+            scroll_view.scrollTo(0, scroll_view.top)
+            create_account_text.visibility = View.VISIBLE
+        }
+    }
+
+    override fun afterTextChanged(p0: Editable?) {
+        login_btn.isEnabled = email_login.text.toString().isNotEmpty() && password_login.text.toString().isNotEmpty()
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+    }
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
     }
 }
